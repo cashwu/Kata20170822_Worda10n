a0151aa3c29dd7004e59d902e54de48aae352c76
cash <cash@cashwu.com>
2017-08-22 14:28:13 +0800

    input are test case

--- Kata20170822_Worda10n/Worda10nTests.cs
+++ Kata20170822_Worda10n/Worda10nTests.cs
@@ -7,8 +7,24 @@ namespace Kata20170822_Worda10n
     public class Worda10nTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_are_should_return_are()
         {
+            AbbreviateShouldBe("are", "are");
+        }
+
+        private static void AbbreviateShouldBe(string expected, string input)
+        {
+            var abbreviator = new Abbreviator();
+            var actual = abbreviator.Abbreviate(input);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class Abbreviator
+    {
+        public string Abbreviate(string input)
+        {
+            return input;
         }
     }
 }
