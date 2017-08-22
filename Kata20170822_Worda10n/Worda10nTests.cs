b49ca9b759429002b661df1c07b712bd42ec80ca
cash <cash@cashwu.com>
2017-08-22 15:49:07 +0800

    input isn't are test case

--- Kata20170822_Worda10n/Worda10nTests.cs
+++ Kata20170822_Worda10n/Worda10nTests.cs
@@ -25,6 +25,12 @@ namespace Kata20170822_Worda10n
             AbbreviateShouldBe("are f5g", "are feeling");
         }
 
+        [TestMethod]
+        public void input_isnt_are_should_return_isnt_are()
+        {
+            AbbreviateShouldBe("isn't are", "isn't are");
+        }
+
         private static void AbbreviateShouldBe(string expected, string input)
         {
             var abbreviator = new Abbreviator();
@@ -43,10 +49,30 @@ namespace Kata20170822_Worda10n
             }
 
             var inputArray = input.Split(' ');
+            var symbol = new[] { ',', '\'', '.' };
+            inputArray = inputArray.Select(item => item.Any(a => symbol.Contains(a)) ? SymbolReviate(item) : Reviate(item)).ToArray();
 
-            inputArray = inputArray.Select(item => item.Length < 4 ? item : $"{item.First()}{item.Length - 2}{item.Last()}").ToArray();
-            
             return string.Join(" ", inputArray);
+        }
+
+        private static string SymbolReviate(string item)
+        {
+            var symbol = new[] { ',', '\'', '.' };
+
+            foreach (var s in symbol)
+            {
+                if (item.Contains(s))
+                {
+                    return string.Join(s.ToString(), item.Split(s).Select(Reviate));
+                }
+            }
+
+            return item;
+        }
+
+        private static string Reviate(string item)
+        {
+            return item.Length < 4 ? item : $"{item.First()}{item.Length - 2}{item.Last()}";
         }
     }
 }
